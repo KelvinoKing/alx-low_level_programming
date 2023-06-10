98e7c5de91ebcaf0c85d570a6d2230fc00ac22fc
KelvinoKing <kelvinoking@outlook.com>
2023-06-10 07:55:27 +0000

    Adds positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point of programe
+ * @argc: number of arguments
+ * @argv: array of argumnets
+ *
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	else if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (*argv[i] >= 48 && *argv[i] <= 57)
+				sum += atoi(argv[i]);
+			else
+			{
+				printf("Error\n");
+				return (0);
+			}
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
