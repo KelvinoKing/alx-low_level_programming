a38c332a98828da6e68112bf83efd79b607bb1f7
KelvinoKing <kelvinoking@outlook.com>
2023-06-15 13:58:23 +0000

    Reallocates a memory block using malloc and free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -27,16 +27,19 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		return (ptr);
 	}
-	if (new_size > old_size)
-	{
-		return (malloc(new_size));
-	}
 
 	ptr_new = malloc(new_size);
 	if (ptr_new == NULL)
 		return (NULL);
-
 	old_mem = ptr;
+	if (new_size > old_size)
+	{
+		while (i < old_size)
+		{
+			ptr_new[i] = old_mem[i];
+			i++;
+		}
+	}
 	if (new_size < old_size)
 	{
 		while (i < new_size)
