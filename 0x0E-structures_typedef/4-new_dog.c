f98d34544526afeabdef0c738a540fbda09d7ae2
KelvinoKing <kelvinoking@outlook.com>
2023-06-19 23:24:29 +0000

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -34,6 +34,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *new_dog;
 	int i = 0, j = 0;
 
+	new_dog->age = age;
+
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
@@ -52,8 +54,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(new_dog);
 		return (NULL);
 	}
-
-	new_dog->age = age;
 
 	while (i < _strlen(name))
 	{
