4e5a3a49ef395d17f91d6507eadd170596e7d894
KelvinoKing <kelvinoking@outlook.com>
2023-06-19 20:53:29 +0000

    Defines a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a struct containing dogs details
+ * @name: holds dog name
+ * @age: holds dog age
+ * @owner: holds owner's name
+ *
+ * Description: contains dog information
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif /*DOG_H*/
