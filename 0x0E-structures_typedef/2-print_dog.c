926ec547694fb5add06b04cb9d451b81566de49c
KelvinoKing <kelvinoking@outlook.com>
2023-06-19 21:54:01 +0000

    Prints a struct dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,10 +16,7 @@ void print_dog(struct dog *d)
 	else if (d->name == NULL)
 		printf("Name: (nil)\n");
 
-	if (d->age != 0)
-		printf("Age: %0.6f\n", d->age);
-	else if (d->age == 0)
-		printf("Age: (nil)\n");
+	printf("Age: %0.6f\n", d->age);
 
 	if (d->owner != NULL)
 		printf("Owner: %s\n", d->owner);
