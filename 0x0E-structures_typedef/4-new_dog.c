494cb67da146d51a2b47bb0bba6207c8249f56f1
KelvinoKing <kelvinoking@outlook.com>
2023-06-19 23:09:28 +0000

    New dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,27 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
- * _strlen - Returns the length of a string
- *
- * @s: Function parameter
- *
- * Return: int
- */
-int _strlen(char *s)
-{
-	int i = 0;
-	int count = 0;
-
-	while (*(s + i) != '\0')
-	{
-		count++;
-		i++;
-	}
-
-	return (count);
-}
-
-/**
  * new_dog - creates a new dog
  * @name: first param
  * @age: second param
@@ -32,40 +11,13 @@ int _strlen(char *s)
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
-	int i = 0;
 
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
 
+	new_dog->name = name;
+	new_dog->owner = owner;
 	new_dog->age = age;
-
-	new_dog->name = malloc(sizeof(new_dog->name) * _strlen(name));
-	if (new_dog->name == NULL)
-	{
-		free(new_dog);
-		return (NULL);
-	}
-
-	new_dog->owner = malloc(sizeof(new_dog->owner) * _strlen(owner));
-	if (new_dog->owner == NULL)
-	{
-		free(new_dog->name);
-		free(new_dog);
-		return (NULL);
-	}
-
-	while (i < _strlen(name))
-	{
-		new_dog->name[i] = name[i];
-		i++;
-	}
-
-	i = 0;
-	while (i < _strlen(owner))
-	{
-		new_dog->owner[i] = owner[i];
-		i++;
-	}
 	return (new_dog);
 }
