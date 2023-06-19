2917e387ad33d854beec98fb777a6eddb5bbce91
KelvinoKing <kelvinoking@outlook.com>
2023-06-19 23:01:15 +0000

    New dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -37,22 +37,23 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
-	new_dog->name = malloc(sizeof(char) * _strlen(name) + 1);
+
+	new_dog->age = age;
+
+	new_dog->name = malloc(sizeof(char) * _strlen(name));
 	if (new_dog->name == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
 
-	new_dog->owner = malloc(sizeof(char) * _strlen(owner) + 1);
+	new_dog->owner = malloc(sizeof(char) * _strlen(owner));
 	if (new_dog->owner == NULL)
 	{
 		free(new_dog->name);
 		free(new_dog);
 		return (NULL);
 	}
-
-	new_dog->age = age;
 
 	while (i < _strlen(name))
 	{
