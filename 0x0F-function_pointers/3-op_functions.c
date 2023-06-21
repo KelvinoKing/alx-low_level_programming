c7b096b7aadea03c87d3b4233be6dd3f2889efb2
KelvinoKing <kelvinoking@outlook.com>
2023-06-21 18:12:43 +0000

    Contains functions that do specified operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,60 @@
+#include "3-calc.h"
+/**
+ * op_add - adds two integers
+ * @a: first param
+ * @b: second param
+ *
+ * Return: int
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts two integers
+ * @a: first param
+ * @b: second param
+ *
+ * Return: int
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies two integers
+ * @a: first param
+ * @b: second param
+ *
+ * Return: int
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides two integers
+ * @a: first param
+ * @b: second param
+ *
+ * Return: int
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - returns remainder of int a
+ * @a: first param
+ * @b: second param
+ *
+ * Return: int
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
