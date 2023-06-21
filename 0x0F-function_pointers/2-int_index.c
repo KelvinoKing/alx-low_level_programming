e8dbc422b98f4ce33e29204316aa572ee0ad61d6
KelvinoKing <kelvinoking@outlook.com>
2023-06-21 12:40:00 +0000

    Searches for an interger

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+/**
+ * int_index - searches for an integer
+ * @array: pointer to array
+ * @size: size of array
+ * @cmp: pointer to function
+ *
+ * Return: int
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int index = 0, i = 0;
+
+	if (array == NULL || cmp == NULL)
+		return (0);
+	if (size <= 0)
+		return (-1);
+
+	while (i < size)
+	{
+		index = cmp(array[i]);
+		if (index != 0)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
