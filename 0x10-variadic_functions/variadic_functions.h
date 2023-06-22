513639e0f202a05ad847d34821d70e13252c3124
KelvinoKing <kelvinoking@outlook.com>
2023-06-22 08:14:21 +0000

    Functions prototypes

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,8 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
+#include <stdlib.h>
+#define NULL ((void *)0)
+int _putchar(char *s);
+int sum_them_all(const unsigned int n, ...);
+#endif /*VARIADIC_FUNCTIONS_H*/
