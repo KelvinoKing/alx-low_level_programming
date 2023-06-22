565a0e5eddd7b3e886ee39134c826d4d866fe5b6
KelvinoKing <kelvinoking@outlook.com>
2023-06-22 08:47:03 +0000

    Prints numbers followed by a new line

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+/**
+ * print_numbers - prints all numbers passed as optional parameters
+ * @separator: string separating numbers
+ * @n: number of parameters
+ *
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	va_list ap;
+
+	if (n == 0)
+		return;
+
+	va_start(ap, n);
+	while (i < n)
+	{
+		printf("%d", va_arg(ap, unsigned int));
+		if ((separator != NULL) && (i < n - 1))
+		{
+			printf("%s", separator);
+		}
+		i++;
+	}
+	va_end(ap);
+	printf("\n");
+}
