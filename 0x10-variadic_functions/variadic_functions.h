d6e6aed2cdf7b735beeb300d44b03a66c1293988
KelvinoKing <kelvinoking@outlook.com>
2023-06-22 08:46:32 +0000

    Functions prototypes

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,7 +3,7 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #define NULL ((void *)0)
-int _putchar(char *s);
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 #endif /*VARIADIC_FUNCTIONS_H*/
