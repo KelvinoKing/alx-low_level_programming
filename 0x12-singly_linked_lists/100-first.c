8bdbde1291fb1d2399c523dfa97a13cc5881a2d9
KelvinoKing <kelvinoking@outlook.com>
2023-07-01 12:05:43 +0000

    Prints a string before main function runs

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * printFirst - Prints a string before main function is executed
+ *
+ * Return: void
+ */
+void printFirst(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
