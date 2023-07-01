e3e8d55ee9be01447f7d8565874875e21448209e
KelvinoKing <kelvinoking@outlook.com>
2023-07-01 08:21:45 +0000

    Adds a new node at the beginning of a list_t list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ * add_node - Adds a new node at beginning of a list_t list
+ * @head: pointer to a pointer of head node
+ * @str: String to be used in new node
+ *
+ * Return: pointer to new node
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *temp_node;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	temp_node = malloc(sizeof(list_t));
+	if (temp_node == NULL)
+		return (NULL);
+
+	temp_node->str = strdup(str);
+	temp_node->len = strlen(temp_node->str);
+	temp_node->next = *head;
+	*head = temp_node;
+
+	return (temp_node);
+}
