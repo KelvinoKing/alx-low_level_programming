b777ffb23b6890951370c89aa8eb536a8268817a
KelvinoKing <kelvinoking@outlook.com>
2023-07-07 07:43:08 +0000

    Returns the value of a bit at a given index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,26 +8,7 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int num = n;
-	unsigned int num_divided = 0, i = 0;
-	int *ptr;
-
-	while (n / 2 > 0)
-	{
-		n /= 2;
-		num_divided++;
-	}
-	num_divided += 1;
-	ptr = malloc(num_divided * sizeof(int));
-	if (ptr == NULL)
+	if (index >= 32)
 		return (-1);
-	while (i < num_divided)
-	{
-		ptr[i] = num % 2;
-		num /= 2;
-		i++;
-	}
-	if (index >= num_divided)
-		return (-1);
-	return (ptr[index]);
+	return ((n >> index) & 1);
 }
