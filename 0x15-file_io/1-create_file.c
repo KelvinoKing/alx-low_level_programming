38869d65a50b67b8f8b4da311d75108e5bc6b7c8
KelvinoKing <kelvinoking@outlook.com>
2023-07-11 06:21:37 +0000

    Creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -29,19 +29,24 @@ int _strlen(char *s)
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, j;
+	int fd, j, len;
 
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
-	if (text_content != NULL)
+	if (text_content == NULL)
 	{
-		j = write(fd, text_content, (_strlen(text_content) + 1));
-		if (j == -1)
-			return (-1);
+		len = 0;
+		text_content = "";
 	}
+	else
+		len = _strlen(text_content);
+
+	j = write(fd, text_content, len);
+	if (j == -1)
+		return (-1);
 	close(fd);
 	return (1);
 }
