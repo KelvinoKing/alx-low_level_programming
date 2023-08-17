83d5bbfc40c9328d1ff8037f32efb9b40e53ba2d
KelvinoKing <kelvinoking@outlook.com>
2023-08-17 12:03:45 +0000

    Returns the number of elemets in the list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - Returns number of elements in a list
+ * @h: pointer to head node
+ *
+ * Return: int
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+	dlistint_t *current;
+
+	if (h != NULL)
+	{
+		if (h->n != 0)
+			count++;
+	}
+	else
+		return (0);
+
+	current = h->next;
+	while (current != NULL)
+	{
+		if (current->n != 0)
+			count++;
+		current = current->next;
+	}
+
+	return (count);
+}
