a7ce615f91292af7cde7c345988b35ab9cdb4ccb
KelvinoKing <kelvinoking@outlook.com>
2023-08-17 12:32:51 +0000

    Adds a node at the beggining of a list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -15,16 +15,16 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
+	new->prev = NULL;
 
-	if (*head == NULL || head == NULL)
+	if (*head == NULL)
 	{
-		new->prev = NULL;
 		new->next = NULL;
 		*head = new;
 		return (new);
 	}
-	new->prev = NULL;
 	new->next = *head;
+	(*head)->prev = new;
 	*head = new;
 
 	return (new);
